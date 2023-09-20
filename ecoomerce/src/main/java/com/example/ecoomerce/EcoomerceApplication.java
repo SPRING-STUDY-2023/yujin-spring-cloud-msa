5c8eaa4ad9b1aa7e2489b1018197af6e08815715
yujindonut <bbb3631@naver.com>
2023-09-20 10:30:04 +0900

    [INIT] 초기셋팅

---
+++ ecoomerce/src/main/java/com/example/ecoomerce/EcoomerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecoomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoomerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoomerceApplication.class, args);
+    }
+
+}
