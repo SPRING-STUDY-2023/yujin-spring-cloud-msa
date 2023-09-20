d61e06bd3f9a7b85a4defcebe92806d92def2cae
yujindonut <bbb3631@naver.com>
2023-09-20 10:33:54 +0900

    init: .gitignore

---
+++ ecoomerce/ecoommerce/src/main/java/com/example/ecoommerce/EcoommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecoommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoommerceApplication.class, args);
+    }
+
+}
---
+++ ecoomerce/ecoommerce/src/test/java/com/example/ecoommerce/EcoommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecoommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
