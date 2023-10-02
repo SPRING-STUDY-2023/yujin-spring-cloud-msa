ba9eac0dcdc477de7fb4f9bea31364a18fa5be19
yujindonut <bbb3631@naver.com>
2023-10-02 16:11:59 +0900

    [ADD] Filter 적용

--- api-gateway/first-service/first-serivce/src/main/java/com/example/firstserivce/FirstServiceController.java
+++ api-gateway/first-service/first-serivce/src/main/java/com/example/firstserivce/FirstServiceController.java
@@ -1,14 +1,23 @@
 package com.example.firstserivce;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@Slf4j
+@RequestMapping("/first-service")
 public class FirstServiceController {
     @GetMapping("/welcome")
     public String welcome() {
+        return "Welcome to the First Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
         return "Welcome to the First Service";
     }
 }
--- api-gateway/second-service/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ api-gateway/second-service/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,14 +1,23 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@Slf4j
+@RequestMapping("/second-service")
 public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
+        return "Welcome to the Second Service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
         return "Welcome to the Second Service";
     }
 }
